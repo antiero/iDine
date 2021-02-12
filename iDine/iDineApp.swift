dd51d814db239d82c92229928aeb8c5802ccb5ca
Antony Nasce <antonynasce@gmail.com>
2021-02-12 17:21:38 +0000

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Antony Nasce on 12/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
