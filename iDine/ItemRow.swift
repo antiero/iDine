2003314f3fa60e3b33b7e7675b22baf6cd310db4
Antony Nasce <antonynasce@gmail.com>
2021-02-12 17:53:57 +0000

    Polishing designs with fonts and colors

--- iDine/ItemRow.swift
+++ iDine/ItemRow.swift
@@ -10,14 +10,31 @@ import SwiftUI
 struct ItemRow: View {
     
     let item: MenuItem
+    let colors: [String: Color] = ["D": .purple, "G": .black, "N": .red, "S": .blue, "V": .green]
+
     
     var body: some View {
         HStack {
             Image(item.thumbnailImage)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.gray, lineWidth: 2))
             
             VStack(alignment: .leading) {
                 Text(item.name)
+                    .font(.headline)
                 Text("$\(item.price)")
+            }
+            
+            Spacer()
+            // ForEach: We have an array of strings - we can’t make them conform to Identifiable. Instead, we need something else: we need to tell Swift that the string itself is the identifier for each item. This can be done using the id parameter for ForEach, passing in \.self as its only parameter
+            ForEach(item.restrictions, id: \.self) { restriction in
+                Text(restriction)
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(5)
+                    .background(colors[restriction, default: .black])
+                    .clipShape(Circle())
+                    .foregroundColor(.white)
             }
         }
     }
